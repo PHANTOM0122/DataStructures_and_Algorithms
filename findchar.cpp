b93e316038ca1360e3f9e6457bf82863a133c124
phatom0122 <wilko97@naver.com>
2021-01-08 23:16:19 +0900

    Create findchar.cpp

---
+++ findchar.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+bool find_char(const char* s, char ch) {
+	while (*s != '\0') {
+		if (*s == ch)
+			return true;
+		s++;
+	}
+	return false;
+}
+
+int main() {
+	const char* phrase = "this is a phrase";
+	for (char ch = 'a'; ch <= 'z'; ch++) {
+		if (find_char(phrase, ch))
+			cout << ch << " is in " << phrase << endl;
+		else
+			cout << ch << " is not in " << phrase << endl;
+	}
+}*/
+
+int main() {
+	int a[] = { 2,3,4 };
+	cout << a;
+}
